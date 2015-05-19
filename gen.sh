a4f3c3b0b96a6a714b0bc6cd783c6c19599c51a4
cgcgbcbc <cgcgbcbc@163.com>
2015-05-20 02:45:40 +0800

    update gen.sh and .gitignore
    resolve #3

--- gen.sh
+++ gen.sh
@@ -4,3 +4,4 @@ if [ "$#" -ne 1 ]; then
 fi
 docker-compose run --rm gen easyrsa build-client-full $1 nopass
 docker-compose run --rm gen ovpn_getclient $1 > $1.ovpn
+sed -i 's/redirect-gateway def1//' $1.ovpn
